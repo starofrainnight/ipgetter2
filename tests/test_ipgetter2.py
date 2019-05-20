81f889946cfb692dab2d73b86aa37a3da9536d34
Hong-She Liang <starofrainnight@gmail.com>
2019-05-20 14:39:25 +0800

    Added first workable test of command line interface

--- tests/test_ipgetter2.py
+++ tests/test_ipgetter2.py
@@ -11,22 +11,6 @@ from ipgetter2 import ipgetter2
 from ipgetter2.__main__ import main
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
