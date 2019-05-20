afc69742c63b2236398171ba19d5e6c73b46e244
Hong-She Liang <starofrainnight@gmail.com>
2019-05-20 17:10:21 +0800

    Test show-servers command

--- tests/test_ipgetter2.py
+++ tests/test_ipgetter2.py
@@ -7,15 +7,18 @@ import pytest
 
 from click.testing import CliRunner
 
-from ipgetter2 import ipgetter2
+from ipgetter2 import ipgetter2, IPGetter
 from ipgetter2.__main__ import main
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(main)
+
+    result = runner.invoke(main, ["show-servers"])
     assert result.exit_code == 0
+
     help_result = runner.invoke(main, ["--help"])
     assert help_result.exit_code == 0
     assert "--help  Show this message and exit." in help_result.output
+
