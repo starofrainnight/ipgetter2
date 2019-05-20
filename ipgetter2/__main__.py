4d3b284568dcbc52e06d9fde27a47d2218e5541f
Hong-She Liang <starofrainnight@gmail.com>
2019-05-20 17:00:14 +0800

    Added command line commands for test and show default servers

--- ipgetter2/__main__.py
+++ ipgetter2/__main__.py
@@ -4,15 +4,68 @@
 """Console script for ipgetter2."""
 
 import click
-from .ipgetter2 import IPGetter, IPAddress
+from .ipgetter2 import IPGetter, IPAddress, DEFAULT_URLS
 
 
-@click.command()
+@click.group()
 def main():
-    """Simple script to get the external ip"""
+    """Simple script for get/test external IP"""
+    pass
+
+
+@main.command()
+def get():
+    """Get the external IP randomly from default servers"""
 
     getter = IPGetter()
     click.echo(getter.get())
+
+
+@main.command()
+def test():
+    """Test default servers"""
+
+    getter = IPGetter()
+    contexts = []
+    failed = []
+    counted = dict()
+
+    i = 0
+    click.echo("%s servers needs to be tested : " % len(DEFAULT_URLS))
+    for url in DEFAULT_URLS:
+        i += 1
+
+        try:
+            click.echo("[%s/%s] Testing : %s" % (i, len(DEFAULT_URLS), url))
+            address = getter.get_from(url)
+            contexts.append((address, url))
+
+            if address in counted:
+                counted[address] += 1
+            else:
+                counted[address] = 1
+        except Exception as e:
+            click.echo(
+                "Test failed on %s, with exception : %s" % (url, e), err=True
+            )
+
+            failed.append(url)
+
+    click.echo("Total urls : %s" % (len(DEFAULT_URLS), DEFAULT_URLS))
+    click.echo("Failed %s urls : %s" % (len(failed), failed))
+
+    click.echo(counted)
+    click.echo(max(counted.items(), lambda item: item[1]))
+
+
+@main.command()
+def show_servers():
+    """Display default servers' urls"""
+
+    i = 0
+    for url in DEFAULT_URLS:
+        click.echo("%s: %s" % (i, url))
+        i += 1
 
 
 if __name__ == "__main__":
--- ipgetter2/ipgetter2.py
+++ ipgetter2/ipgetter2.py
@@ -114,6 +114,9 @@ class IPAddress(object):
     def is_valid(self):
         return bool(self.v4 or self.v6)
 
+    def __hash__(self):
+        return hash(tuple([self.v4, self.v6]))
+
     def __repr__(self):
         return '%s(v4="%s", v6="%s")' % (
             self.__class__.__name__,
