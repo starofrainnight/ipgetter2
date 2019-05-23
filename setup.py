d07b1e28747ef007f0f7feb16d5bd5344ccd9a49
Hong-She Liang <starofrainnight@gmail.com>
2019-05-23 11:57:18 +0800

    Fixed dependences not installed during pip install

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ with io.open("README.rst", encoding="utf-8") as readme_file, io.open(
 ) as history_file:
     long_description = readme_file.read() + "\n\n" + history_file.read()
 
-install_requires = ["click>=6.0"]
+install_requires = ["click>=6.0", "requests", "chardet"]
 
-setup_requires = ["pytest-runner", "requests", "chardet"]
+setup_requires = ["pytest-runner"]
 
 tests_requires = [
     "pytest",
